743fce3d8374c1cb72beddb870f5666a2cd8919f
mahsakhazaei <63585423+mahsakhazaei@users.noreply.github.com>
2024-11-15 18:09:36 +0330

    Delete Exception directory

--- Exception/src/main/java/ir/freeland/exception/chained/CustomFileNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package ir.freeland.exception.chained;
-
-class CustomFileNotFoundException extends Exception {
-	private static final long serialVersionUID = 1L;
-
-	public CustomFileNotFoundException(String message, Throwable cause) {
-        super(message, cause); // Pass the message and cause to the parent Exception class
-    }
-}
